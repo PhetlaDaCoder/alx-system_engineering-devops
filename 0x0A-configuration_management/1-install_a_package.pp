213d698163bf07ebba79dea977b4cb2ced270109
PhetlaDaCoder <hsphetla@gmail.com>
2025-01-22 09:25:10 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 #Using Puppet, install flask from pip3
-package{'puppet-lint':
-ensure   => '2.1.0',
-provider => 'gem'
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
 }
