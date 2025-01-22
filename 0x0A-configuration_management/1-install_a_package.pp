513afefa4592055bea344bd741bf4de688217542
PhetlaDaCoder <hsphetla@gmail.com>
2025-01-22 09:28:11 -0800

    2-execute_a_command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,1 @@
-#Using Puppet, install flask from pip3
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/pip3 show flask | grep "Version: 2.1.0"',
-}
+
