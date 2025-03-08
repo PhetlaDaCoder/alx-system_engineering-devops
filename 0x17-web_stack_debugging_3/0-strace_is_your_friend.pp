d1e9c11edffbef10cef33aa81151be44ede4d727
PhetlaDaCoder <hsphetla@gmail.com>
2025-03-08 12:14:14 -0800

    updated

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fixing Apache returning a 500 error
-
-exec { 'fix error':
-  provider => 'shell',
-  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
+# edits typing error of .phpp to php
+exec { 'fix-wordpress-server-error':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/bin/:/bin/',
 }
